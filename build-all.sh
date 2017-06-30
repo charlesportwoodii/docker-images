1791e7b9fa5d9a69716a324fe651322ae66c825e
Charles R. Portwood II <charlesportwoodii@erianna.com>
2017-06-30 16:01:46 +0000

    [ci skip] adding helper build script

---
+++ build-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+for i in xenial centos7 rhel7 trusty; do
+    for j in nginx php56 php70 php71 disque; do
+        if [ -d ./$i/$j ]; then
+            echo "Building $i/$j"
+            docker build $i/$j
+        fi
+    done
+done
